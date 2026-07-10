af880764e542449dbb95c008b210a07f1b255737
Kushagra Raghav <152775429+Kushagraraghav@users.noreply.github.com>
2026-07-10 22:41:05 +0530

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -2,17 +2,20 @@ const fs = require("fs");
 
 const stats = JSON.parse(fs.readFileSync("stats.json", "utf8"));
 
-const total =
-  stats.easy + stats.medium + stats.hard;
+const easy = stats.leetcode.easy;
+const medium = stats.leetcode.medium;
+const hard = stats.leetcode.hard;
+
+const total = easy + medium + hard;
 
 const section = `
 ## 📊 GitHub Upload Stats
 
 | Difficulty | Count |
 |------------|------:|
-| 🟢 Easy | ${stats.easy} |
-| 🟡 Medium | ${stats.medium} |
-| 🔴 Hard | ${stats.hard} |
+| 🟢 Easy | ${easy} |
+| 🟡 Medium | ${medium} |
+| 🔴 Hard | ${hard} |
 | **Total Uploaded** | **${total}** |
 
 _Last Updated Automatically_
@@ -23,13 +26,17 @@ let readme = fs.readFileSync("README.md", "utf8");
 const start = "<!-- STATS_START -->";
 const end = "<!-- STATS_END -->";
 
-const regex = new RegExp(
-  `${start}[\\s\\S]*${end}`
-);
+const regex = new RegExp(`${start}[\\s\\S]*${end}`);
 
 readme = readme.replace(
-  regex,
-  `${start}\n${section}\n${end}`
+    regex,
+`${start}
+
+${section}
+
+${end}`
 );
 
 fs.writeFileSync("README.md", readme);
+
+console.log("README Updated Successfully!");
